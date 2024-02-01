3d697f2327b54104106695d31b4611969f8a5d54
akashdevcc <akashdevcc@gmail.com>
2024-02-01 15:45:34 +0530

    refactor(native): :recycle: Component "Post" moved to "native" package.

--- apps/expo/src/app/(main)/post/[id].tsx
+++ apps/expo/src/app/(main)/post/[id].tsx
@@ -1,24 +1,15 @@
-import { SafeAreaView, Text, View } from "react-native";
-import { Stack, useGlobalSearchParams } from "expo-router";
+import { useGlobalSearchParams } from "expo-router";
+
+import { Post } from "@acme/native";
 
 import { api } from "~/utils/api";
 
-export default function Post() {
+export default function () {
   const { id } = useGlobalSearchParams();
   if (!id || typeof id !== "string") throw new Error("unreachable");
   const { data } = api.post.byId.useQuery({ id: parseInt(id) });
 
   if (!data) return null;
 
-  return (
-    <SafeAreaView className="bg-background">
-      <Stack.Screen options={{ title: data.title }} />
-      <View className="h-full w-full p-4">
-        <Text className="py-2 text-3xl font-bold text-primary">
-          {data.title}
-        </Text>
-        <Text className="py-4 text-foreground">{data.content}</Text>
-      </View>
-    </SafeAreaView>
-  );
+  return <Post title={data.title} content={data.content} />;
 }
--- packages/native/src/index.ts
+++ packages/native/src/index.ts
@@ -1,1 +1,3 @@
-export const name = "native";
+import { Post } from "./components";
+
+export { Post };
