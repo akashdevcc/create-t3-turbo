3af480af59dab228e4a5b0700b87faec7ee1fe25
akashdevcc <akashdevcc@gmail.com>
2024-02-01 11:50:01 +0530

    refactor(web): :recycle: Package "ui" references changed to "web".

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata, Viewport } from "next";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
-import { cn } from "@acme/ui";
-import { ThemeProvider, ThemeToggle } from "@acme/ui/theme";
-import { Toaster } from "@acme/ui/toast";
+import { cn } from "@acme/web";
+import { ThemeProvider, ThemeToggle } from "@acme/web/theme";
+import { Toaster } from "@acme/web/toast";
 
 import { env } from "~/env";
 import { TRPCReactProvider } from "~/trpc/react";
