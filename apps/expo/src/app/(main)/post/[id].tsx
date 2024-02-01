7c3f2340cb733245fd5d74137b9a5ef76e2e64c6
akashdevcc <akashdevcc@gmail.com>
2024-02-01 16:45:40 +0530

    perf(expoapp): :zap: LocalSearchParams used with static type.
    useGlobalSearchParams replaced with statically-typed useLocalSearchParams for performance increase.

--- apps/expo/src/app/(main)/post/[id].tsx
+++ apps/expo/src/app/(main)/post/[id].tsx
@@ -1,12 +1,11 @@
-import { useGlobalSearchParams } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 
 import { Post } from "@acme/native";
 
 import { api } from "~/utils/api";
 
 export default function () {
-  const { id } = useGlobalSearchParams();
-  if (!id || typeof id !== "string") throw new Error("unreachable");
+  const { id } = useLocalSearchParams<{ id: string }>();
   const { data } = api.post.byId.useQuery({ id: parseInt(id) });
 
   if (!data) return null;
