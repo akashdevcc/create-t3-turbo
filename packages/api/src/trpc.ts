761c469301ada13191328d518423760797836141
akashdevcc <akashdevcc@gmail.com>
2024-02-02 19:49:32 +0530

    refactor(api): :recycle: options header removed from api context creation

--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -6,7 +6,8 @@
  * tl;dr - this is where all the tRPC server stuff is created and plugged in.
  * The pieces you will need to use are documented accordingly near the end
  */
-import { initTRPC, TRPCError } from "@trpc/server";
+import { initTRPC } from "@trpc/server";
+// import { TRPCError } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
 
@@ -24,10 +25,10 @@ import { db } from "@acme/db";
  *
  * @see https://trpc.io/docs/server/context
  */
-export const createTRPCContext = async (opts: { headers: Headers }) => {
-  const source = opts.headers.get("x-trpc-source") ?? "unknown";
+export const createTRPCContext = async () => {
+  // const source = opts.headers.get("x-trpc-source") ?? "unknown";
 
-  console.log(">>> tRPC Request from", source);
+  // console.log(">>> tRPC Request from", source);
 
   return {
     db,
