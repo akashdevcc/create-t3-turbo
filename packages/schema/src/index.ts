9fcb720c3ff306248a35a2590f69de4b2fce6e03
akashdevcc <akashdevcc@gmail.com>
2024-02-05 13:17:09 +0530

    refactor(schema): :recycle: Reference of mySqlTableCreator renamed.

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -2,4 +2,4 @@ import * as post from "./schema/post";
 
 export const schema = { ...post };
 
-export { mySqlTable as tableCreator } from "./schema/_table";
+export { tableCreator } from "./schema/_table";
